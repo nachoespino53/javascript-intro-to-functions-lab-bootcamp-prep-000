978dd9e4ee24d0f93eb86cc7158eaed52494c0ae
Ignacio R Espino <irespino@capozios.com>
2018-08-01 14:46:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase) return "YES INDEED!";
+  if (string === string.toUpperCase()) return "YES INDEED!";
   if(string === 'I love you, Grandma.') return "I love you, too.";
  }
